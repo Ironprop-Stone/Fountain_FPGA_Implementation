7374f5e873e69cf9d8b19a1cdd5779efb4781b98
Stone <zyshi21@cse.cuhk.edu.hk>
2022-08-31 00:42:30 +0800

    first commit

---
+++ dma_dac.sdk/da/Debug/src/dma_da.d
@@ -0,0 +1,90 @@
+src/dma_da.o src/dma_da.o: ../src/dma_da.c \
+ ../../da_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../da_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../da_bsp/ps7_cortexa9_0/include/ad9708_send.h ../src/wave.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../da_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../da_bsp/ps7_cortexa9_0/include/sleep.h
+
+../../da_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../da_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../da_bsp/ps7_cortexa9_0/include/ad9708_send.h:
+
+../src/wave.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../da_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../da_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ dma_dac.sdk/fsbl/Debug/src/ps7_init.d
@@ -0,0 +1,32 @@
+src/ps7_init.o src/ps7_init.o: \
+ /home/zhengyuan/src/FPGA_Studio/10_ad9708_dma/dma_dac.sdk/top_wrapper_hw_platform_0/ps7_init.c \
+ /home/zhengyuan/src/FPGA_Studio/10_ad9708_dma/dma_dac.sdk/top_wrapper_hw_platform_0/ps7_init.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+/home/zhengyuan/src/FPGA_Studio/10_ad9708_dma/dma_dac.sdk/top_wrapper_hw_platform_0/ps7_init.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
